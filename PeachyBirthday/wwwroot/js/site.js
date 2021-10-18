cf431f6b11f96e109a3611fe17191492e77a5211
Dan <daniel.giddins112@gmail.com>
2021-10-18 20:11:33 +0100

    format all files

--- PeachyBirthday/wwwroot/js/site.js
+++ PeachyBirthday/wwwroot/js/site.js
@@ -1,8 +1,7 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-  new CircleType(document.getElementById('top-text'))
-    .radius(1000);
-  new CircleType(document.getElementById('bottom-text'))
-    .dir(-1)
-    .radius(1000);
+	new CircleType(document.getElementById('top-text'))
+		.radius(1000);
+	new CircleType(document.getElementById('bottom-text'))
+		.dir(-1)
+		.radius(1000);
 }, false);
-
