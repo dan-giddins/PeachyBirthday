28f4d51cfa9e4e6820e4bc317c82ea7692278965
Dan <daniel.giddins112@gmail.com>
2021-10-18 16:37:19 +0100

    sway

--- PeachyBirthday/wwwroot/js/site.js
+++ PeachyBirthday/wwwroot/js/site.js
@@ -1,5 +1,8 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
   new CircleType(document.getElementById('top-text'))
     .radius(1000);
+  new CircleType(document.getElementById('bottom-text'))
+    .dir(-1)
+    .radius(1000);
 }, false);
 
